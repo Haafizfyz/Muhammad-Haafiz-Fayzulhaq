0563c4176ab02b27104606de223496a1383597fa
Your Name <you@example.com>
2022-11-08 07:21:09 +0700

    kelas.py

---
+++ kelas.py
@@ -0,0 +1,20 @@
+#soal 1
+print ('hello world to python')
+
+#soal 2
+print (10 + 10)
+print (10 - 5)
+print (5 * 2)
+print (10 // 2)
+
+#soal 3
+#int#
+var1 = 10
+var2 = 2
+print (type(10 // 2))
+print (type(var1 // var2))
+print (type(var1 / var2))
+
+usia = "16 tahun"
+kata = "usiaku"
+print ("{} {}". format (kata,usia))
